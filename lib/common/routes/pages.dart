63cff9b8805e3b91c70c4af09699e8153e4ceeb0
Dereck Z <zavaladereck77@gmail.com>
2023-06-05 21:20:38 -0600

    If applied, this commit starts the message module and brings the Route Auth middleware.

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -1,9 +1,9 @@
 import 'package:get/get.dart';
 
-import 'package:nameless_chat/common/middlewares/middlewares.dart';
 import 'package:nameless_chat/common/routes/routes.dart';
 import 'package:nameless_chat/pages/frame/message/index.dart';
 import 'package:nameless_chat/pages/frame/welcome/index.dart';
+import 'package:nameless_chat/common/middlewares/middlewares.dart';
 
 class AppPages {
   static const initial = AppRoutes.initial;
